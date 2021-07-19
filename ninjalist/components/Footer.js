ca0c0df7c20f4d864e732aa0c8dcfb68fb569d57
Logan Negley <logannegley@gmail.com>
2021-07-19 16:21:20 -0500

    global css add

--- ninjalist/components/Footer.js
+++ ninjalist/components/Footer.js
@@ -1,9 +1,9 @@
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <p>Copyright 2021 Ninja List</p>
-        </div>
+        </footer>
     )
 }
 
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -2,19 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/Link'
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 
 
 export default function Home() {
   return (
     <div >
-      <Navbar/>
       <h1>HomePage</h1>
       <Link href='/ninjas'>
         <a>See Ninja Listing</a>
       </Link>
-      <Footer/>
     </div>
   )
 }

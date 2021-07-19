d3c51b9ca2d3b09475c367f21349cf1a9641727c
Logan Negley <logannegley@gmail.com>
2021-07-19 16:01:44 -0500

    navbar and footer added

---
+++ ninjalist/components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyright 2021 Ninja List</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ ninjalist/pages/about.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae alias fugiat consectetur perferendis incidunt aliquid quibusdam autem soluta repellendus! Maiores quasi aliquid quis quo ex facere minus. Eos illo dolor ab earum ducimus architecto asperiores, neque inventore! Eligendi, alias. Fuga corporis assumenda dolor consequuntur vel. Sapiente nesciunt aspernatur obcaecati itaque.</p>
+        </div>
+    )
+}
+
+export default About;
